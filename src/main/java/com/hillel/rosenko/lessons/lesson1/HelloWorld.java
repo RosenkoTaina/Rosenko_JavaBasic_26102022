f29f77ff22171006273f2f6cf6968043bec62f27
RosenkoTaina <rosenko.taya@gmail.com>
2022-11-09 22:06:24 +0200

    New

---
+++ src/main/java/com/hillel/rosenko/lessons/lesson1/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.hillel.rosenko.lessons.lesson1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world\n");
+        System.out.println("bye world");
+
+    }
+}
