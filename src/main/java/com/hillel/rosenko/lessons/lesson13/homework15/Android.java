4a5b15304a97047923fffd0297827d331c07b685
Rosenko Taina <rosenko.taya@gmail.com>
2023-01-08 11:45:53 +0200

    Homework 15 (changed)

--- src/main/java/com/hillel/rosenko/lessons/lesson13/homework15/Android.java
+++ src/main/java/com/hillel/rosenko/lessons/lesson13/homework15/Android.java
@@ -3,7 +3,7 @@ package com.hillel.rosenko.lessons.lesson13.homework15;
 public class Android extends Smartphone implements Smartphones {
     public Android (String name, String model) {
         super(name, model);
-        this.os = 0;
+        this.os = LINUXES;
     }
 
 }
