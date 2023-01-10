3713ef47bf11c1e9723c4a2595ff675cb440545a
Rosenko Taina <rosenko.taya@gmail.com>
2023-01-10 21:27:13 +0200

    Homework 17

--- src/main/java/com/hillel/rosenko/lessons/lesson14/homework17/Homework17.java
+++ src/main/java/com/hillel/rosenko/lessons/lesson14/homework17/Homework17.java
@@ -1,7 +1,6 @@
 package com.hillel.rosenko.lessons.lesson14.homework17;
 
 import java.util.Arrays;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Homework17 {
@@ -9,12 +8,10 @@ public class Homework17 {
 
         runApp();
 
-
     }
 
 
-
-    public static void runApp() {
+    public static void runApp() throws IllegalStateException {
 
         Scanner scanner = new Scanner(System.in);
         DrinksMachine[] drinksMachines = DrinksMachine.values();
@@ -23,77 +20,80 @@ public class Homework17 {
         System.out.println("Please enter drinks: " + Arrays.toString(drinksMachines));
 
         double price = 0;
-
-        while (true) {
+        boolean stop = false;
+        while (!stop) {
             try {
                 if (scanner.hasNextLine()) {
-                    String userValue = scanner.nextLine();
+                    String userValue = String.valueOf(scanner.nextLine());
                     userValue = userValue.toUpperCase();
 
                     for (DrinksMachine drinksMachine : drinksMachines) {
                         if (drinksMachine.toString().equals(userValue)) {
                             userDrinkType = DrinksMachine.valueOf(userValue);
+                        } else if (userValue.equals("EXIT")) {
+                            System.out.println("count drinks: " + Counter.getCounter());
+                            System.out.println("Total: " + price);
+                            stop = true;
+                            break;
+                        } else if (userDrinkType == null){
+                            System.out.println("Unexpected value ___: " );
+                            break;
                         }
                     }
-                } else {
-                    System.out.println("Невірне значення");
-                    scanner.next();
                 }
 
-            } catch (InputMismatchException e) {
-                System.out.println("Невірне значення");
-                scanner.next();
+                if (userDrinkType != null) {
+                    switch (userDrinkType) {
+                        case COFFEE: {
+                            new Counter();
+                            System.out.println("Your order " + DrinksMachine.COFFEE.getDrink());
+                            price += DrinksMachine.COFFEE.getCost();
+                            break;
+                        }
+                        case TEA: {
+                            new Counter();
+                            System.out.println("Your order " + DrinksMachine.TEA.getDrink());
+                            price += DrinksMachine.TEA.getCost();
+                            break;
+                        }
+                        case LEMONADE: {
+                            new Counter();
+                            System.out.println("Your order " + DrinksMachine.LEMONADE.getDrink());
+                            price += DrinksMachine.LEMONADE.getCost();
+                            break;
+                        }
+
+                        case MOJITO: {
+                            new Counter();
+                            System.out.println("Your order " + DrinksMachine.MOJITO.getDrink());
+                            price += DrinksMachine.MOJITO.getCost();
+                            break;
+                        }
+                        case MINERAL_WATER: {
+                            new Counter();
+                            System.out.println("Your order " + DrinksMachine.MINERAL_WATER.getDrink());
+                            price += DrinksMachine.MINERAL_WATER.getCost();
+                            break;
+                        }
+                        case COCA_COLA: {
+                            new Counter();
+                            System.out.println("Your order " + DrinksMachine.COCA_COLA.getDrink());
+                            price += DrinksMachine.COCA_COLA.getCost();
+                            break;
+                        }
+                        default:
+                            throw new IllegalArgumentException("Invalid operation");
+                    }
+                }
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-            switch (userDrinkType) {
-                case COFFEE: {
-                    new Counter();
-                    System.out.println("Your order " + DrinksMachine.COFFEE.getDrink());
-                    price += DrinksMachine.COFFEE.getCost();
-                    System.out.println("Your total " +  price);
-                    break;
-                }
-                case TEA: {
-                    new Counter();
-                    System.out.println("Your order " + DrinksMachine.TEA.getDrink());
-                    price += DrinksMachine.TEA.getCost();
-                    System.out.println("Your total " +  price);
-                    break;
-                }
-                case LEMONADE: {
-                    new Counter();
-                    System.out.println("Your order " + DrinksMachine.LEMONADE.getDrink());
-                    price += DrinksMachine.LEMONADE.getCost();
-                    System.out.println("Your total " +  price);
-                    break;
-                }
-
-                case MOJITO: {
-                    new Counter();
-                    System.out.println("Your order " + DrinksMachine.MOJITO.getDrink());
-                    break;
-                }
-                case MINERAL_WATER: {
-                    new Counter();
-                    System.out.println("Your order " + DrinksMachine.MINERAL_WATER.getDrink());
-                    break;
-                }
-                case COCA_COLA: {
-                    new Counter();
-                    System.out.println("Your order " + DrinksMachine.COCA_COLA.getDrink());
-                    break;
-                }
-                case STOP: {
-                    System.out.println("Your order ");
-                    System.out.println("count drinks: " + Counter.getCounter());
-                    System.out.println("Total: " + price);
-                    break;
-                }
-
-
-            }
-
         }
     }
 }
+
+
+
 
 
